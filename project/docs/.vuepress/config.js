afa2f3f29621c1fa429472ffb70ba004a06e849c
xushuohao <shuohao.xu@weimob.com>
2019-06-19 17:30:19 +0800

    no message

--- project/docs/.vuepress/config.js
+++ project/docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: 'Chen\'s blog',
+    title: 'Hao\'s blog',
     description: '我的个人网站',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
@@ -12,7 +12,7 @@ module.exports = {
       nav:[ // 导航栏配置
         {text: '前端基础', link: '/accumulate/' },
         {text: '算法题库', link: '/algorithm/'},
-        {text: '微博', link: 'https://baidu.com'}      
+        {text: '微博', link: 'https://baidu.com'}
       ],
       sidebar: 'auto', // 侧边栏配置
       sidebarDepth: 2, // 侧边栏显示2级
